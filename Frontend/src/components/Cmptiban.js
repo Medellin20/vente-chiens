93dbaff4411bb3568b4f6867350f27b759394356
Medellin <zola-eric.wendeou@epitech.eu>
2024-02-01 10:45:24 +0100

    update

---
+++ Frontend/src/components/Cmptiban.js
@@ -0,0 +1,44 @@
+// PaymentPage.js
+import React, { useState } from 'react';
+// import { useParams } from 'react-router-dom';
+//import axios from 'axios';
+import './Cmptiban.css'; // Importez le fichier CSS
+
+const Cmptiban = () => {
+
+  //const token = localStorage.getItem('token');
+
+  const [displayRIB, setDisplayRIB] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDisplayRIB("RIB : 1235 4587 4596 2541")
+      
+  };
+
+  return (
+    <div className="payment-container">
+      <form className="payment-form" onSubmit={handleSubmit}>
+      <label>
+      <h2 className='zola'> Paiement sécurisé : </h2>
+            
+      <p>  Faites un virement depuis votre compte bancaire vers votre compte HOMEDOG (RIB ci-dessous). 
+            Vous recevrez un message de confirmation dès réception des fonds. 
+            La réception des fonds peut prendre jusqu’à 48h ouvrées. 
+            Pour accélérer la démarche, demandez un virement express auprès de votre banquier.</ p>
+          
+        
+        </label>   
+
+        
+        <button type="submit" className="payment-button" onClick={handleSubmit}>Afficher le RIB pour le paiement</button>
+      </form>
+    
+      <p> <h3> {displayRIB} </h3>  </p>
+    </div>
+  );
+};
+
+export default Cmptiban;
---
+++ Frontend/src/dogsData.js
@@ -0,0 +1,38 @@
+// src/dogsData.js
+const dogsData = [
+    {  breed: "Berger Allemand", price: 750, image: "berger1.png", caption: "Age: 1an Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Allemand", price: 675, image: "berger2.png", caption: "Age: 1an 2mois Sexe: Mâle Castré: Non  Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Carlos et Perla", price: 1400, image: "berger3.png", caption: "Age: 1an et 11mois Sexe: Mâle et Femelle  Castré(e): Non  Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Allemand", price: 700, image: "berger4.png", caption: "Age: 3mois Sexe: Femelle Castrée: Non Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Malinois Pure sang", price: 700, image: "berger5.png", caption: "Age: 2mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Malinois croisé", price: 550, image: "berger6.png", caption: "Age: 1mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand Pure Sang", price: 700, image: "berger7.png", caption:  "Age: 1mois Sexe: Femelle Castrée: Non Poils: Long Carnet de vaccination: A jour"},
+    {  breed: "Berger Malinois", price: 600, image: "berger8.png", caption: "Age: 1mois Sexe: Mâle Castré: Oui Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Allemand", price: 650, image: "berger9.png", caption: "Age: 1mois Sexe: Mâle Castré: Oui Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 600, image: "berger10.png", caption: "Age: 1an 4mois Sexe: Mâle Castré: Oui Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Allemand", price: 700, image: "berger11.png", caption: "Age: 4mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    { breed: "Berger Allemand", price: 800, image: "berger12.png", caption: "Age: 1mois Sexe: Femelle Castré: Non Poils: Longs  Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 650, image: "berger13.png", caption: "Age: 3mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 700, image: "berger14.png", caption: "Age: 1mois Sexe: Mâle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 800, image: "berger15.png", caption: "Age: 3mois Sexe: Femelle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 700, image: "berger16.png", caption: "Age: 2mois Sexe: Femelle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 650, image: "berger17.png", caption: "Age: 1mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour"   },
+    {  breed: "Berger Allemand", price: 750, image: "berger18.png", caption: "Age: 1an Sexe: Femelle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 750, image: "berger19.png", caption: "Age: 3mois Sexe: Mâle Castré: Oui Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 650, image: "berger20.png", caption: "Age: 2mois Sexe: Mâle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 750, image: "berger21.png", caption: "Age: 7mois Sexe: Femelle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 650, image: "berger22.png", caption: "Age: 1an Sexe: Mâle Castré: Oui Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 650, image: "berger23.png", caption: "Age: 6mois Sexe: Femelle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand et Berger Malinois", price: 650 & 750, image: "berger24.png", caption: "Age: 6mois et 8 mois Sexe: Mâle Castré: Non Poils: Longs  Carnet de vaccination: A jour" },
+     {  breed: "Berger Allemand", price: 700, image: "berger25.png", caption: "Age: 9mois Sexe: Mâle Castré: Oui Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 650, image: "berger26.png", caption: "Age: 11mois Sexe: Mâle Castré: Oui Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 750, image: "berger27.png", caption: "Age: 2mois Sexe: Mâle Castré: Non Poils: Courts Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 550, image: "berger28.png", caption: "Age: 7mois Sexe: Femelle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Malinois", price: 600, image: "berger29.png", caption: "Age: 2mois Sexe: Mâle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+    {  breed: "Berger Allemand", price: 700, image: "berger30.png", caption: "Age: 8mois Sexe: Femelle Castré: Non Poils: Longs Carnet de vaccination: A jour" },
+   
+    // ... autres chiens"Un sympathique Labrador
+  ];
+  
+  export default dogsData;
+ 
